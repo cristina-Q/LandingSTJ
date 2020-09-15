3f8ec1bd1f957132df43c54361fb6abbed22b904
cristina-q <cristinamihaela@yandex.com>
2020-09-15 17:25:27 +0300

    load from server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
 
-const PORT = 8080;
+const hostname = '127.0.0.1';
+const port = 3000;
 
-fs.readFile('./index.html', function (err, html) {
-  if (err) throw err;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  fs.createReadStream('index.html').pipe(res);
+});
 
-  http
-    .createServer(function (request, response) {
-      response.writeHeader(200, { 'Content-Type': 'text/html' });
-      response.write(html);
-      response.end();
-    })
-    .listen(PORT);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
